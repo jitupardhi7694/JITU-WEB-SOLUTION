f0d43f35dc4b8248201efa9ab2bfc3c63034f6c4
JITENDRA PARDHI <jitupardhi7694>
2023-08-02 18:29:24 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const expressLayout = require('express-ejs-layouts');
+const port = 3000;
+
+app.use(expressLayout);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('layout', 'layout/main_layout');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', require('./routes/indexRoute'));
+
+app.listen(port, (error) => {
+    if (error) throw error;
+    else console.log('listening on port http://localhost:3000');
+});
